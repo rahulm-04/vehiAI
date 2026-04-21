fa12c541b8c3efa0035aacf862a58106dde90b8f
rahulm-04 <raahulix01@gmail.com>
2026-04-21 17:01:58 +0530

    disable turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -2,33 +2,33 @@ import { Value } from '@radix-ui/react-select';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{
-        serverComponentsHmrCache:false,
-    },
+  experimental: {
+    serverComponentsHmrCache: false,
+    turbo: false, // ✅ ADD THIS LINE
+  },
 
-    images:{
-        remotePatterns:[
-            {
-                protocol:"https",
-                hostname:"aurlihswjhzjcozcoyvs.supabase.co",
-            },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "kufahfzqwtarzjsaynfd.supabase.co",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "content-security-policy",
+            value: "frame-src 'self' https://raahulix44.created.app;",
+          },
         ],
-    },
-
-
-    async headers(){
-        return[
-            {
-                source:"/embed",
-                headers:[
-                   { 
-                    key:"content-security-policy",
-                    value:"frame-src 'self' https://raahulix44.created.app;",
-                   }
-                ]
-            }
-        ]
-    }
+      },
+    ];
+  },
 };
 
 export default nextConfig;
